acb50790a44b31208b25dbcc5c162d3b90a89806
Manuel Garnica <manuel.garnica@elconde.org>
2019-04-16 15:17:01 -0500

    primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CalendarModule} from 'primeng/calendar';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+//import {SplitButtonModule} from 'primeng/splitbutton';
+import {InputMaskModule} from 'primeng/inputmask';
+import {GMapModule} from 'primeng/gmap';
+//import {MenuItem} from 'primeng/api';  
+import {FileUploadModule} from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+	,FormsModule
+	,AccordionModule
+	,BrowserAnimationsModule
+	,CalendarModule
+	,InputMaskModule
+	,GMapModule
+	,FileUploadModule
+	,HttpClientModule
+	
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
